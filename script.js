438102455248e6b21bbd55c5a49fbe754345c913
SIPHONIUM <siphonium.official@gmail.com>
2025-08-07 09:02:27 +0200

    enleve le cache du site
    ca enleve le cache du site pour que le css se recharge a chaque fois

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 (function () {
   const baseURL = "https://siphonium.github.io/";
-
   const supportedLangs = ["fr", "en", "es", "de", "it", "jp", "pt", "ar", "ru", "zh"];
-  
-  let userLang = (navigator.language || navigator.userLanguage || "en").toLowerCase();
-  let langCode = userLang.split("-")[0]; 
 
-  if (supportedLangs.includes(langCode)) {
-    window.location.replace(`${baseURL}${langCode}/`);
-  } else {
-    window.location.replace(`${baseURL}en/`); 
+  if ('caches' in window) {
+    caches.keys().then(function(names) {
+      for (let name of names) caches.delete(name);
+    });
   }
+
+  let userLang = (navigator.language || navigator.userLanguage || "en").toLowerCase();
+  let langCode = userLang.split("-")[0];
+
+  let redirectLang = supportedLangs.includes(langCode) ? langCode : "en";
+
+  const timestamp = `?t=${Date.now()}`;
+  window.location.replace(`${baseURL}${redirectLang}/${timestamp}`);
 })();
